1322a61a1fe59c169e06343e7293a46249e61a1f
garryod <garry.o'donnell@diamond.ac.uk>
2022-01-17 13:37:11 +0000

    Add guarded model admin views to admin panel

--- antigenapi/admin.py
+++ antigenapi/admin.py
@@ -1,9 +1,41 @@
 from django.contrib.admin import site
+from guardian.admin import GuardedModelAdmin
 
 from antigenapi.models import Antigen, ElisaPlate, ElisaWell, Nanobody, Sequence
 
-site.register(Antigen)
-site.register(Nanobody)
-site.register(ElisaPlate)
-site.register(ElisaWell)
-site.register(Sequence)
+
+class AntigenAdmin(GuardedModelAdmin):
+    """A guarded model admin view for antigen objects."""
+
+    pass
+
+
+class NanobodyAdmin(GuardedModelAdmin):
+    """A guarded model admin view for nanobody objects."""
+
+    pass
+
+
+class ElisaPlateAdmin(GuardedModelAdmin):
+    """A guarded model admin view for elisa plate objects."""
+
+    pass
+
+
+class ElisaWellAdmin(GuardedModelAdmin):
+    """A guarded model admin view for elisa well objects."""
+
+    pass
+
+
+class SequenceAdmin(GuardedModelAdmin):
+    """A guarded model admin view for sequence objects."""
+
+    pass
+
+
+site.register(Antigen, AntigenAdmin)
+site.register(Nanobody, NanobodyAdmin)
+site.register(ElisaPlate, ElisaPlateAdmin)
+site.register(ElisaWell, ElisaWellAdmin)
+site.register(Sequence, SequenceAdmin)
