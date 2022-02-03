2e526dc96f16cf81ea4e41377795fd0806716be2
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-03 16:47:14 +0000

    Added loading & error UI elements

---
+++ frontend/src/utils/api.tsx
@@ -0,0 +1,22 @@
+import { Card, LinearProgress, Paper, Typography } from "@mui/material"
+
+export function fromAPI(uriFrag: string) {
+    return fetch(`http://127.0.0.1:8000/api/${uriFrag}/?format=json`)
+}
+
+export const LoadingPaper = (params: { text: string }) => {
+    return (
+        <Paper>
+            <Typography>{params.text}</Typography>
+            <LinearProgress />
+        </Paper>
+    )
+}
+
+export const FailedRetrievalPaper = (params: { text: string }) => {
+    return (
+        <Paper>
+            <Typography variant="h5">{params.text}</Typography>
+        </Paper>
+    )
+}
