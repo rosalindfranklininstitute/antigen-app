6d76a645f2ac0e9557a6835d91489456d7a1e77f
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-03 15:34:31 +0000

    Update antigen views

--- frontend/src/main/antigen.tsx
+++ frontend/src/main/antigen.tsx
@@ -1,10 +1,15 @@
-import { Paper, TableCell, TableContainer, TableHead, TableRow, Table, TableBody } from "@mui/material";
+import { Paper, TableCell, TableContainer, TableHead, TableRow, Table, TableBody, Typography, Card } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 type Antigen = {
     uuid: string
+    sequence: string
+    molecular_mass: number
+    name: string
+    uniprot_accession_number: string
     antigen_elisa_wells: Array<string>
+    creation_time: Date
 };
 
 function AntigenView() {
@@ -23,22 +28,37 @@ function AntigenView() {
         fetchAntigen();
     }, []);
 
-    console.log(antigen);
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>UUID:</TableCell>
-                        <TableCell>{antigen?.uuid}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Elisa Appearances:</TableCell>
-                        <TableCell>{antigen?.antigen_elisa_wells}</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Card>
+            <Typography variant="h4">{antigen?.name}</Typography>
+            <Typography variant="h5">({antigen?.uuid})</Typography>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Sequence:</TableCell>
+                            <TableCell>{antigen?.sequence}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Molecular Mass:</TableCell>
+                            <TableCell>{antigen?.molecular_mass}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Uniprot Accession Number:</TableCell>
+                            <TableCell>{antigen?.uniprot_accession_number}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Elisa Appearances:</TableCell>
+                            <TableCell>{antigen?.antigen_elisa_wells}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Creation Time:</TableCell>
+                            <TableCell>{antigen?.creation_time}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Card>
     )
 };
 
@@ -61,14 +81,20 @@ function AntigensView() {
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell>UUID</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Molecular Mass</TableCell>
+                        <TableCell>Associated Wells</TableCell>
+                        <TableCell>Creation Time</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {
-                        antigens.map((e, i) => (
-                            <TableRow key={i}>
-                                <TableCell><Link to={`/antigen/${e.uuid}`}>{e.uuid}</Link></TableCell>
+                        antigens.map((antigen, idx) => (
+                            <TableRow key={idx}>
+                                <TableCell><Link to={`/antigen/${antigen.uuid}`}>{antigen.name}</Link></TableCell>
+                                <TableCell>{antigen.molecular_mass}</TableCell>
+                                <TableCell>{antigen.antigen_elisa_wells.length}</TableCell>
+                                <TableCell>{antigen.creation_time}</TableCell>
                             </TableRow>
                         ))
                     }
