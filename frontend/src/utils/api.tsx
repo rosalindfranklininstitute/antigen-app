32b63690434e6adfb517eaaa8bb71026aec38c28
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-04 13:29:50 +0000

    Add postAPI utility function

--- frontend/src/utils/api.tsx
+++ frontend/src/utils/api.tsx
@@ -4,6 +4,22 @@ export function getAPI(uriFrag: string) {
     return fetch(`http://127.0.0.1:8000/api/${uriFrag}/?format=json`)
 }
 
+export function postAPI(uriFrag: string, post: object) {
+    return fetch(
+        `http://127.0.0.1:8000/api/${uriFrag}/`,
+        {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        }
+    )
+}
+
 export const LoadingPaper = (params: { text: string }) => {
     return (
         <Paper>
