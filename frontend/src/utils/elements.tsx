bc14dc5b4baeb82e8566239dcf78926855cbcec7
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-07 13:33:01 +0000

    Add LinkUUIDCellRenderer to element utils
    Simplify interface of LinkUUIDCellRenderer

--- frontend/src/utils/elements.tsx
+++ frontend/src/utils/elements.tsx
@@ -1,7 +1,23 @@
-import { Button, Menu, MenuItem, MenuList } from "@mui/material";
+import { Button, Link, Menu, MenuItem, MenuList } from "@mui/material";
 import { GridRenderCellParams } from "@mui/x-data-grid";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+export const LinkUUID = (params: { rootURI: string, UUID: string }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Link
+            onClick={() => navigate(`${params.rootURI}${params.UUID}`)}
+        >
+            {params.UUID}
+        </Link>
+    )
+}
+export const LinkUUIDCellRenderer = (rootURI: string) => {
+    return (params: GridRenderCellParams<string>) => <LinkUUID rootURI={rootURI} UUID={params.value} />
+}
+
 
 export const WellCellRenderer = (params: GridRenderCellParams<string[]>) => {
     const navigate = useNavigate();
