a35a72b14f34b2e750d830beb08b0238efb9b5f3
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-07 14:41:43 +0000

    Add util element for link icon button cells

--- frontend/src/utils/elements.tsx
+++ frontend/src/utils/elements.tsx
@@ -1,7 +1,8 @@
-import { Button, Link, Menu, MenuItem, MenuList } from "@mui/material";
-import { GridRenderCellParams } from "@mui/x-data-grid";
+import { Button, IconButton, Link, Menu, MenuItem, MenuList } from "@mui/material";
+import { GridRenderCellParams, GridColDef } from "@mui/x-data-grid";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import LinkIcon from '@mui/icons-material/Link';
 
 export const LinkUUID = (params: { rootURI: string, UUID: string }) => {
     const navigate = useNavigate();
@@ -18,6 +19,30 @@ export const LinkUUIDCellRenderer = (rootURI: string) => {
     return (params: GridRenderCellParams<string>) => <LinkUUID rootURI={rootURI} UUID={params.value} />
 }
 
+export const IconLinkUUID = (params: { rootURI: string, UUID: string }) => {
+    const navigate = useNavigate();
+
+    return (
+        <IconButton
+            onClick={() => navigate(`${params.rootURI}${params.UUID}`)}
+        >
+            <LinkIcon />
+        </IconButton>
+    )
+}
+
+export const IconLinkUUIDCellRenderer = (rootURI: string) => {
+    return (params: GridRenderCellParams<string>) => <IconLinkUUID rootURI={rootURI} UUID={params.value} />
+}
+
+export const IconLinkUUIDGridColDef = (rootURI: string): GridColDef => {
+    return {
+        field: 'uuid',
+        headerName: 'Link',
+        renderCell: IconLinkUUIDCellRenderer(rootURI),
+        width: 50
+    }
+}
 
 export const WellCellRenderer = (params: GridRenderCellParams<string[]>) => {
     const navigate = useNavigate();
