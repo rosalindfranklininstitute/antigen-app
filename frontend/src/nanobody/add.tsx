c597fffc798a1dca1ef5c9f0c55e713e9119c834
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-14 17:03:30 +0000

    Use filter posted in nanobody add

--- frontend/src/nanobody/add.tsx
+++ frontend/src/nanobody/add.tsx
@@ -1,19 +1,16 @@
 import { Card, CardContent, Typography, Stack, Divider, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import SendIcon from '@mui/icons-material/Send';
-import { Nanobody, NanobodyInfo } from "./utils";
+import { NanobodyInfo } from "./utils";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useDispatch, useSelector } from "react-redux";
 import { nanobodySelector, postNanobody } from "./slice";
+import { filterPosted } from "../utils/state_management";
 
 export default function AddNanobodyView() {
     const dispatch = useDispatch();
     const { nanobodies, posted, loading } = useSelector(nanobodySelector);
-    const postedNanobodies = posted.map(
-        (postUUID) => nanobodies.find(
-            (nanobody) => nanobody.uuid === postUUID
-        )
-    ).filter((nanobody): nanobody is Nanobody => !!nanobody)
+    const postedNanobodies = filterPosted(nanobodies, posted);
 
     const submit = async () => {
         dispatch(postNanobody());
