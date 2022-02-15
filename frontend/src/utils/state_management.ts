8efd246d842b7c3a6e477eb07ae086dce379f431
garryod <garry.o'donnell@diamond.ac.uk>
2022-02-15 09:07:58 +0000

    Give new objects precidence when adding by uuid

--- frontend/src/utils/state_management.ts
+++ frontend/src/utils/state_management.ts
@@ -1,8 +1,8 @@
 export function addUniqueUUID<Type extends { uuid: string }>(oldObjs: Type[], newObjs: Type[]) {
-    return oldObjs.concat(
-        newObjs.filter(
-            (newObj) => !oldObjs.find(
-                (oldObj) => oldObj.uuid === newObj.uuid
+    return newObjs.concat(
+        oldObjs.filter(
+            (oldObj) => !newObjs.find(
+                (newObj) => newObj.uuid === oldObj.uuid
             )
         )
     )
