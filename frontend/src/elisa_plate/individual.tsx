9d48e10f930cfc6bbf9322eb7fd9cc753780c5a9
garryod <garry.o'donnell@diamond.ac.uk>
2022-02-15 13:28:31 +0000

    Use redux to manage elisa plate state

--- frontend/src/elisa_plate/individual.tsx
+++ frontend/src/elisa_plate/individual.tsx
@@ -1,32 +1,23 @@
 import { Card, CardContent, Stack, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { LoadingPaper, FailedRetrievalPaper } from "../utils/api";
-import { DetailedElisaPlate, ElisaPlateInfo, fetchDetailedElisaPlate } from "./utils";
+import { getDetailedElisaPlate, selectDetailedElisaPlate, selectLoadingDetailedElisaPlate } from "./slice";
+import { ElisaPlateInfo } from "./utils";
 import { ElisaWellMapElement } from "./well_map";
 
 export default function ElisaPlateView() {
-    let { uuid } = useParams<{ uuid: string }>();
-
-    const [detailedElisaPlate, setDetailedElisaPlate] = useState<DetailedElisaPlate | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
+    let { uuid } = useParams<{ uuid: string }>() as { uuid: string };
+    const dispatch = useDispatch();
+    const detailedElisaPlate = useSelector(selectDetailedElisaPlate(uuid));
+    const loading = useSelector(selectLoadingDetailedElisaPlate);
 
     useEffect(() => {
-        if (!uuid) {
-            setLoading(false);
-            return;
-        };
-        fetchDetailedElisaPlate(uuid).then(
-            (detailedElisaPlate) => {
-                setDetailedElisaPlate(detailedElisaPlate);
-                setLoading(false);
-            },
-            () => setLoading(false)
-        )
-    }, [uuid]);
+        dispatch(getDetailedElisaPlate(uuid));
+    }, [dispatch, uuid]);
 
     if (loading) return <LoadingPaper text="Retrieving elisa plate from database." />
-
     if (!detailedElisaPlate) return <FailedRetrievalPaper text={`Could not retrieve entry for ${window.location.href.split("/").pop()}`} />
 
     return (
