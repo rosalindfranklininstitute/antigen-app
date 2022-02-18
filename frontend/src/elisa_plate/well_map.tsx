ab9d027da694b641bb7d26bd77339114a313916b
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-02-18 12:02:24 +0000

    Add well edit UI element

--- frontend/src/elisa_plate/well_map.tsx
+++ frontend/src/elisa_plate/well_map.tsx
@@ -1,15 +1,103 @@
-import { Typography, Grid, Button, Paper, Popover, Stack } from "@mui/material";
+import { Typography, Grid, Button, Paper, Popover, Stack, CardContent, Card, Autocomplete, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAntigen, selectAntigen } from "../antigen/slice";
+import { getAntigen, getAntigens, selectAntigen, selectAntigens } from "../antigen/slice";
 import { getElisaWell, selectElisaWell } from "../elisa_well/slice";
 import { ElisaWell, locationToCoords } from "../elisa_well/utils";
-import { getNanobody, selectNanobody } from "../nanobody/slice";
+import { getNanobodies, getNanobody, selectNanobodies, selectNanobody } from "../nanobody/slice";
 import { RootState } from "../store";
 
 function uuidToColor(uuid: string) {
     const hue = Number("0x".concat(uuid.substring(0, 2)))
     return `hsl(${hue}, 50%, 50%)`;
+}
+
+function ElisaWellEditPopover(params: { uuid: string, anchorEl: HTMLElement | null, setAnchorEl: (anchorEl: (HTMLElement | null)) => void }) {
+    const dispatch = useDispatch();
+    const elisaWell = useSelector(selectElisaWell(params.uuid));
+    const antigens = useSelector(selectAntigens);
+    const nanobodies = useSelector(selectNanobodies);
+
+    useEffect(() => {
+        dispatch(getElisaWell(params.uuid));
+        dispatch(getAntigens());
+        dispatch(getNanobodies());
+    }, [dispatch, params])
+
+    return (
+        < Popover
+            open={Boolean(params.anchorEl)}
+            anchorEl={params.anchorEl}
+            anchorOrigin={{
+                vertical: "center",
+                horizontal: "center",
+            }}
+            transformOrigin={{
+                vertical: "center",
+                horizontal: "center",
+            }}
+            onClose={() => params.setAnchorEl(null)}
+        >
+            <Card>
+                <CardContent>
+                    <Stack spacing={2}>
+                        <Autocomplete
+                            renderInput={(params) => <TextField {...params} label="Antigen" sx={{ width: "32ch" }} />}
+                            options={antigens}
+                            getOptionLabel={(antigen) => antigen.name}
+                            defaultValue={antigens.find((antigen) => antigen.uuid === elisaWell?.antigen)}
+                        />
+                        <Autocomplete
+                            renderInput={(params) => <TextField {...params} label="Nanobody" sx={{ width: "32ch" }} />}
+                            options={nanobodies}
+                            getOptionLabel={(nanobody) => nanobody.name}
+                            defaultValue={nanobodies.find((nanobody) => nanobody.uuid === elisaWell?.nanobody)}
+                        />
+                    </Stack>
+                </CardContent>
+            </Card>
+        </Popover >
+    )
+}
+
+function ElisaWellInfoPopover(params: { uuid: string, anchorEl: HTMLElement | null, setAnchorEl: (anchorEl: (HTMLAnchorElement | null)) => void }) {
+    const dispatch = useDispatch();
+    const elisaWell = useSelector(selectElisaWell(params.uuid));
+    const antigen = useSelector(elisaWell ? selectAntigen(elisaWell.antigen) : () => undefined);
+    const nanobody = useSelector(elisaWell ? selectNanobody(elisaWell.nanobody) : () => undefined);
+
+    useEffect(() => {
+        dispatch(getElisaWell(params.uuid));
+    });
+
+    useEffect(() => {
+        if (elisaWell) {
+            dispatch(getAntigen(elisaWell.antigen));
+            dispatch(getNanobody(elisaWell.nanobody));
+        }
+    }, [dispatch, elisaWell]);
+
+    return (
+        <Popover
+            open={Boolean(params.anchorEl)}
+            anchorEl={params.anchorEl}
+            anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+            }}
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+            }}
+            onClose={() => params.setAnchorEl(null)}
+            sx={{ pointerEvents: "none" }}
+        >
+            <Stack alignItems="center">
+                {antigen && <div>Antigen: {antigen.name}</div>}
+                {nanobody && <div>Nanobody: {nanobody.name}</div>}
+            </Stack>
+        </Popover>
+    )
 }
 
 function ElisaWellElement(params: { uuid: string | null }) {
@@ -32,29 +120,11 @@ function ElisaWellElement(params: { uuid: string | null }) {
     const antigenColor = antigen ? uuidToColor(antigen.uuid) : "white";
     const nanobodyColor = nanobody ? uuidToColor(nanobody.uuid) : "white";
 
-    const [infoAnchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+    const [infoAnchorEl, setInfoAnchorEl] = useState<HTMLElement | null>(null);
+    const [editAnchorEl, setEditAnchorEl] = useState<HTMLElement | null>(null);
 
-    const InfoPopover = () => elisaWell ? (
-        <Popover
-            open={Boolean(infoAnchorEl)}
-            anchorEl={infoAnchorEl}
-            anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "center",
-            }}
-            transformOrigin={{
-                vertical: "top",
-                horizontal: "center",
-            }}
-            onClose={() => setAnchorEl(null)}
-            sx={{ pointerEvents: "none" }}
-        >
-            <Stack alignItems="center">
-                {antigen && <div>Antigen: {antigen.name}</div>}
-                {nanobody && <div>Nanobody: {nanobody.name}</div>}
-            </Stack>
-        </Popover>
-    ) : null
+    const InfoPopover = () => params.uuid ? <ElisaWellInfoPopover uuid={params.uuid} anchorEl={infoAnchorEl} setAnchorEl={setInfoAnchorEl} /> : null
+    const EditPopover = () => params.uuid ? <ElisaWellEditPopover uuid={params.uuid} anchorEl={editAnchorEl} setAnchorEl={setEditAnchorEl} /> : null;
 
     return (
         <Paper
@@ -68,10 +138,15 @@ function ElisaWellElement(params: { uuid: string | null }) {
                     width: "100%",
                     padding: "0 0 100% 0",
                 }}
-                onMouseEnter={(evt) => setAnchorEl(evt.currentTarget)}
-                onMouseLeave={() => setAnchorEl(null)}
+                onMouseEnter={(evt) => setInfoAnchorEl(evt.currentTarget)}
+                onMouseLeave={() => setInfoAnchorEl(null)}
+                onClick={(evt) => {
+                    setInfoAnchorEl(null);
+                    setEditAnchorEl(evt.currentTarget);
+                }}
             />
             <InfoPopover />
+            <EditPopover />
         </Paper>
     )
 };
@@ -91,7 +166,7 @@ export function ElisaWellMapElement(params: { uuids: string[] }) {
     }, [dispatch, params])
 
     const elisaWellGrid: (ElisaWell | null)[][] = new Array(8).fill(undefined).map(
-        (val, idx) => new Array(12).fill(null)
+        () => new Array(12).fill(null)
     );
 
     elisaWells.forEach((elisaWell) => {
