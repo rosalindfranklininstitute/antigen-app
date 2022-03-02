c498b6fc85d78f7f6f2d94ba7677d3cded7e1ee4
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-03-02 10:40:58 +0000

    Add elisa plate creation functionality

---
+++ frontend/src/elisa_plate/add.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { DispatchType } from "../store";
+import { LoadingPaper } from "../utils/api";
+import { postElisaPlate } from "./slice";
+
+export default function AddElisaPlateView() {
+  const dispatch = useDispatch<DispatchType>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(postElisaPlate(0.0)).then((uuid) =>
+      navigate(`/elisa_plate/${uuid}/`)
+    );
+  }, [dispatch, navigate]);
+
+  return <LoadingPaper text={"Adding new elisa plate to database."} />;
+}
