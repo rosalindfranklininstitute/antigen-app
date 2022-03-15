dbfdfa5fb56bebe59f8996014461ec699d50458f
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-03-15 10:20:46 +0000

    Remove index tests

--- frontend/src/main/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from ".";
-
-test("renders header", () => {
-  render(<App />);
-  const headerText = screen.getByText(/Antigen App/);
-  expect(headerText).toBeInTheDocument();
-});
