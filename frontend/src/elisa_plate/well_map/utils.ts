e0a8d7f3da7d692766e3abd3048a2aa68acc7685
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-03-23 14:11:23 +0000

    Fix well colour generation

--- frontend/src/elisa_plate/well_map/utils.ts
+++ frontend/src/elisa_plate/well_map/utils.ts
@@ -1,13 +1,11 @@
 function hashNumber(number: number): number {
-  number = ((number >> 16) ^ number) * 0x45d9f3b;
-  number = ((number >> 16) ^ number) * 0x45d9f3b;
-  return (number >> 16) ^ number;
+  number = ((number >>> 16) ^ number) * 0x45d9f3b;
+  number = ((number >>> 16) ^ number) * 0x45d9f3b;
+  return (number >>> 16) ^ number;
 }
 
-export function objToColor(
-  obj: { project: string; number: number } | undefined
-) {
-  return obj !== undefined
-    ? `hsl(${hashNumber(obj.number)}, 50%, 50%)`
+export function numToColor(number: number | undefined) {
+  return number !== undefined
+    ? `hsl(${((hashNumber(number) % 360) + 360) % 360}, 50%, 50%)`
     : "white";
 }
