f3385973d8a667706bd1c1b830bb84f242c93dbb
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-03-25 12:01:48 +0000

    Comment elisa well utils
    Refactor elisa well slice
    
    Comment elisa well individual
    
    Comment elisa well aggregate

--- frontend/src/elisa_well/utils.tsx
+++ frontend/src/elisa_well/utils.tsx
@@ -30,6 +30,14 @@ export type ElisaWellRef = Pick<ElisaWell, "project" | "plate" | "location">;
 export type ElisaWellPost = ElisaWellRef &
   Pick<ElisaWell, "antigen" | "nanobody" | "optical_density">;
 
+/**
+ *
+ * Deserializes elisa well reference URIs from their string representation,
+ * producing an elisa well reference containing a project, plate and location
+ *
+ * @param elisaWellRef The serialized elisa well reference URI string
+ * @returns An elisa well reference containing a project, plate and location
+ */
 export function deserializeElisaWellRef(elisaWellRef: string): ElisaWellRef {
   const [project, plate_str, location_str] = elisaWellRef.split(":") as [
     string,
@@ -43,10 +51,29 @@ export function deserializeElisaWellRef(elisaWellRef: string): ElisaWellRef {
   };
 }
 
+/**
+ *
+ * Serializes elisa well reference URIs from elisa well reference objects
+ * containing a project, plate and location
+ *
+ * @param elisaWellRef An elisa well reference containing a project, plate and
+ * location
+ * @returns The serialized elisa well reference URI string
+ */
 export function serializeElisaWellRef(elisaWellRef: ElisaWellRef): string {
-  return `${elisaWellRef.project}:${elisaWellRef.plate}:${elisaWellRef.location}`;
+  return `${elisaWellRef.project}
+  :${elisaWellRef.plate}
+  :${elisaWellRef.location}`;
 }
 
+/**
+ *
+ * Produces a tuple of row and column from the row-major location of an item in
+ * a 2D grid
+ *
+ * @param location The row-major location of an item in a grid
+ * @returns A tuple of row and column indices
+ */
 function locationToCoords(location: number): [number, number] {
   return [
     Math.floor((location.valueOf() - 1) / 12),
@@ -54,11 +81,32 @@ function locationToCoords(location: number): [number, number] {
   ];
 }
 
-export function locationToGrid(location: number) {
+/**
+ *
+ * Produces a tuple of row and column from the row-major location of an item in
+ * a 2D grid, where the row is represented as a character indexed from A
+ *
+ * @param location The row-major location of an item in a grid
+ * @returns A tuple of row letter and column number
+ */
+export function locationToGrid(location: number): [string, number] {
   const [row, col] = locationToCoords(location);
-  return [String.fromCharCode(65 + row).concat((col + 1).toString())];
+  return [String.fromCharCode(65 + row), col + 1];
 }
 
+/**
+ *
+ * A table of elisa well information, containing rows for the plate, location,
+ * antigen, nanobody, optical density and creation time. Elisa well information
+ * is retrieved from the redux store with a dispatch executed to obtain it if
+ * unavailable
+ *
+ * @param params A elisa well reference from which the elisa well can be
+ * reteieved
+ * @param params.elisaWellRef The elisa well reference
+ * @returns A MUI table with rows containing the plate, location, antigen,
+ * nanobody, optical density and creation time
+ */
 export function ElisaWellInfo(params: { elisaWellRef: ElisaWellRef }) {
   const dispatch = useDispatch();
   const elisaWell = useSelector(selectElisaWell(params.elisaWellRef));
@@ -119,16 +167,28 @@ export function ElisaWellInfo(params: { elisaWellRef: ElisaWellRef }) {
   );
 }
 
-export const ElisaWellRefStack = (elisaWellRefs: Array<ElisaWellRef>) => (
-  <Stack>
-    {elisaWellRefs.map((elisaWellRef, idx) => (
-      <Link
-        component={RouterLink}
-        to={`/elisa_well/${serializeElisaWellRef(elisaWellRef)}`}
-        key={idx}
-      >
-        {serializeElisaWellRef(elisaWellRef)}
-      </Link>
-    ))}
-  </Stack>
-);
+/**
+ *
+ * A list of links to individual elisa well views from an array of elisa well
+ * references
+ *
+ * @param elisaWellRefs An array of elisa well references
+ * @returns A list of links to individual elisa well views
+ */
+export function ElisaWellRefStack(
+  elisaWellRefs: Array<ElisaWellRef>
+): JSX.Element {
+  return (
+    <Stack>
+      {elisaWellRefs.map((elisaWellRef, idx) => (
+        <Link
+          component={RouterLink}
+          to={`/elisa_well/${serializeElisaWellRef(elisaWellRef)}`}
+          key={idx}
+        >
+          {serializeElisaWellRef(elisaWellRef)}
+        </Link>
+      ))}
+    </Stack>
+  );
+}
