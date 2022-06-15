94a1d7ee0d900317b7237c500de14275f3103be6
Alex Lubbock <code@alexlubbock.com>
2022-06-15 19:34:51 +0100

    Move Django static URL to avoid clash with react

--- backend/antigendjango/settings.py
+++ backend/antigendjango/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "djangostatic/"
 
 STATIC_ROOT = BASE_DIR.joinpath("static")
 
