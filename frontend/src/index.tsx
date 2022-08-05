79e85cda5b64a383bd7eddf3ff7d0a9d0c8e5e4e
Alex Lubbock <code@alexlubbock.com>
2022-08-05 14:29:53 +0100

    Integrate sentry into frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,20 @@ import App from "./main/index";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./store";
 import { SnackbarNotifier } from "./utils/notifications";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+if (process.env.REACT_APP_SENTRY_DSN !== undefined) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
