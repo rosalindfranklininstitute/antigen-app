ddb40ef80504d2ee0ab8fa05d6ee6e5bb914679b
Alex Lubbock <alex.lubbock@rfi.ac.uk>
2023-03-19 23:19:08 +0000

    feat: capture user email from oauth2-proxy into db

--- backend/antigendjango/auth.py
+++ backend/antigendjango/auth.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.backends import RemoteUserBackend
 from django.contrib.auth.middleware import RemoteUserMiddleware
 
 
@@ -5,3 +6,20 @@ class RFIRemoteUserMiddleware(RemoteUserMiddleware):
     """Idenitfy user by HTTP header."""
 
     header = "HTTP_X_AUTH_REQUEST_PREFERRED_USERNAME"
+
+
+class RFIRemoteUserBackend(RemoteUserBackend):
+    """Custom backend to populate user profile."""
+
+    def configure_user(self, request, user, created=True):
+        """Capture user's email into database if provided by oauth2-proxy."""
+        if not request:
+            return user
+
+        email = request.headers.get("X-Auth-Request-Email")
+
+        if email and user.email != email:
+            user.email = email
+            user.save()
+
+        return user
--- backend/antigendjango/settings.py
+++ backend/antigendjango/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 }
 
 AUTHENTICATION_BACKENDS = (
-    "django.contrib.auth.backends.RemoteUserBackend",
+    "antigendjango.auth.RFIRemoteUserBackend",
     "guardian.backends.ObjectPermissionBackend",
 )
 
